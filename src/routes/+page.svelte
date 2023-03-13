897f377d5a49e854f343b21f62ad2435ca62948d
Filip Dombos <filip.dombos@gmail.com>
2023-03-13 15:08:25 +0100

    add TailwindCSS and SkeletonUI to dependencies, update config files such as tailwind.config.cjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<h1 />
+
+<AppBar>
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+	<svelte:fragment slot="headline">Welcome to SvelteKit</svelte:fragment>
+</AppBar>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Welcome Optimates, this is our frontend.</p>
