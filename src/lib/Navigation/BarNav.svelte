1a2b5822b766b290247e2f2a4dfc2e247da187cc
Andy Aidoo <andy.aidoo@uzh.ch>
2023-04-13 13:50:03 +0200

    sticky bar nav on the bottom

--- src/lib/Navigation/BarNav.svelte
+++ src/lib/Navigation/BarNav.svelte
@@ -15,7 +15,7 @@
 	flex="flex-1 lg:flex-none"
 	rounded=""
 	border=""
-	class="bg-surface-100-800-token w-full"
+	class="bg-surface-100-800-token w-full fixed inset-x-0 bottom-0 z-40"
 >
 	<Tab bind:group={$tabSet} name="Home" value={0} on:click={() => goto('/')}>
 		<svelte:fragment slot="lead">(icon)</svelte:fragment>
