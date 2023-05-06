257554833e26475fa10b79662a7944824c4ea445
Filip Dombos <filip.dombos@gmail.com>
2023-05-06 17:27:00 +0200

    fix redis client file

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -10,7 +10,3 @@ await client.set('key1', 'This is a test!!!!!!!');
 const value = await client.get('key1');
 console.log(value);
 await client.disconnect();
-
-
-git config --global user.email "filip.dombos@gmail.com"
-git config --global user.name "Filip Dombos"
