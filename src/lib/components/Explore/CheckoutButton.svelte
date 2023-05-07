b36959982610a549852084bebd52e3adbb23917f
Filip Dombos <filip.dombos@gmail.com>
2023-05-07 02:49:45 +0200

    make Checkout button disapear when no items are in the basket

--- src/lib/components/Explore/CheckoutButton.svelte
+++ src/lib/components/Explore/CheckoutButton.svelte
@@ -17,13 +17,15 @@
 	});
 </script>
 
-<div class="flex justify-center w-full">
-	<button class="btn variant-filled-primary relative" on:click={handleClick}>
-		Checkout
-		<span
-			class="bg-white text-black rounded-full w-6 h-6 flex items-center justify-center absolute -top-2 -right-2"
-		>
-			{favCount}
-		</span>
-	</button>
-</div>
+{#if favCount}
+	<div class="flex justify-center w-full">
+		<button class="btn variant-filled-primary relative" on:click={handleClick}>
+			Checkout
+			<span
+				class="bg-white text-black rounded-full w-6 h-6 flex items-center justify-center absolute -top-2 -right-2"
+			>
+				{favCount}
+			</span>
+		</button>
+	</div>
+{/if}
