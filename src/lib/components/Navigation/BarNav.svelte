790319aef97c2a5ec7d8f37afc81f0249a5d7539
Filip Dombos <filip.dombos@gmail.com>
2023-05-07 02:38:27 +0200

    fix reload issue with Navbar, stays on current path

--- src/lib/components/Navigation/BarNav.svelte
+++ src/lib/components/Navigation/BarNav.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { Tab, TabGroup } from '@skeletonlabs/skeleton';
+	import type { Page } from '@sveltejs/kit';
+	import { page } from '$app/stores';
+	import { onDestroy } from 'svelte';
 
-	import { writable, type Writable } from 'svelte/store';
+	let tab: number | undefined = 0;
 
-	const tabSet: Writable<number> = writable(0);
+	const pathMapping = (path: string | undefined) => {
+		switch (path) {
+			case '':
+				return 0;
+			case 'explore':
+				return 1;
+			case 'profile':
+				return 2;
+		}
+	};
+
+	const unsubscribe = page.subscribe((page) => {
+		let route = page.route.id;
+		tab = pathMapping(route?.split('/')[1]);
+	});
+
+	const handleRedirect = (link: string) => {
+		if (link) {
+			goto(link);
+		} else {
+			throw Error;
+		}
+	};
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
 <!-- TODO highlighting of current does not fully work, when user is being redirected -->
@@ -18,16 +46,28 @@
 	border=""
 	class="bg-surface-100-800-token w-full"
 >
-	<Tab bind:group={$tabSet} name="Home" value={0} on:click={() => goto('/')}>
-		<svelte:fragment slot="lead"><img src="/icons/home.svg" alt="home icon" /></svelte:fragment>
-		Home
+	<Tab bind:group={tab} name="Home" value={0} on:click={() => handleRedirect('/')}>
+		<svelte:fragment slot="lead">
+			<img class="block mx-auto" src="icons/home.svg" alt="home icon" />
+		</svelte:fragment>
+		<p class="text-center">Home</p>
 	</Tab>
-	<Tab bind:group={$tabSet} name="Explore" value={1} on:click={() => goto('/explore')}>
-		<svelte:fragment slot="lead"><img src="/icons/compass.svg" alt="home icon" /></svelte:fragment>
-		Explore
+	<Tab bind:group={tab} name="Explore" value={1} on:click={() => handleRedirect('/explore')}>
+		<svelte:fragment slot="lead">
+			<img class="block mx-auto" src="icons/compass.svg" alt="compass icon" />
+		</svelte:fragment>
+		<p class="text-center">Explore</p>
 	</Tab>
-	<Tab bind:group={$tabSet} name="My" value={2} on:click={() => goto('/profile')}>
-		<svelte:fragment slot="lead"><img src="/icons/users.svg" alt="home icon" /></svelte:fragment>
-		My
+	<Tab bind:group={tab} name="My" value={2} on:click={() => handleRedirect('/profile')}>
+		<svelte:fragment slot="lead">
+			<img class="block mx-auto" src="icons/users.svg" alt="users icon" />
+		</svelte:fragment>
+		<p class="text-center">My</p>
 	</Tab>
 </TabGroup>
+
+<style>
+	.tab {
+		@apply flex flex-col justify-center;
+	}
+</style>
