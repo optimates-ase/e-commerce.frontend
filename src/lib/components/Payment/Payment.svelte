34db9a84e7193dee9a1e840a04fcfb463b3c4a0c
Filip Dombos <filip.dombos@gmail.com>
2023-05-13 23:32:15 +0200

    small adaption before rebase

--- src/lib/components/Payment/Payment.svelte
+++ src/lib/components/Payment/Payment.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import { stripe } from '$lib/stripe';
+	import { stripe_elements } from '$lib/stripe';
 
-	console.log(stripe);
+	console.log(stripe_elements);
 </script>
 
-<div class="w-full border">Payment</div>
+<div class="w-full border">
+	<div>Payment</div>
+</div>
