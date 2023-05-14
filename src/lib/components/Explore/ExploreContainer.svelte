fa25ee6da01e33351cc7652535e3b81e9fbcd43f
Filip Dombos <filip.dombos@gmail.com>
2023-05-14 18:13:58 +0200

    fixing changes with pulling new data when near the end of the scroll array

--- src/lib/components/Explore/ExploreContainer.svelte
+++ src/lib/components/Explore/ExploreContainer.svelte
@@ -4,21 +4,36 @@
 
 	import type { Tour } from '$lib/types';
 	import ExploreCard from '$comp/Explore/ExploreCard.svelte';
+	import { children } from 'svelte/internal';
 
 	export let tours: Tour[];
+	let SwipeComp;
+	let currentIndex = 0;
+	const handleSlideChange = (event: any) => {
+		console.log(event.active_item);
+	};
+	$: console.log(currentIndex);
 
 	const swipeConfig = {
 		autoplay: false,
 		delay: 0,
 		showIndicators: false,
 		transitionDuration: 500,
-		defaultIndex: 0
+		defaultIndex: 0,
+		callback: function (index: number) {
+			currentIndex = index;
+		}
 	};
 </script>
 
 <div class="swipe-holder">
-	<Swipe {...swipeConfig} is_vertical={true}>
-		{#each tours as tour}
+	<Swipe
+		{...swipeConfig}
+		is_vertical={true}
+		bind:this={SwipeComp}
+		on:change={() => handleSlideChange}
+	>
+		{#each tours as tour, index}
 			<SwipeItem>
 				<ExploreCard {tour} />
 			</SwipeItem>
